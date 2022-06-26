e3a6cd8c4c6cd254997173866933ae27f9fb9075
Lee Tran <lee.tran95@yahoo.com>
2022-06-25 18:51:36 -0700

    Simple ff14 class api

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const { request } = require('express');
+app.use(cors());
+
+const classes = {
+    'paladin': {
+        'weapon': 'sword and shield',
+        'role': 'tank'
+    },
+    'warrior': {
+        'weapon': 'greataxe',
+        'role': 'tank'
+    },
+    'dark knight': {
+        'weapon':  'greatsword',
+        'role': 'tank'
+    },
+    'gunbreaker': {
+        'weapon': 'gunblade',
+        'role': 'tank'
+    }
+}
+
+app.listen(8000,()=>{
+    console.log("FF14 Server Started")
+})
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name',(req,res)=>{
+    const className = req.params.name.toLowerCase();
+    console.log(className)
+    if(classes[className]){
+        res.json(classes[className])
+    }else{
+        res.json(classes)
+    }
+})
